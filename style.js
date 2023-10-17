bc0433f91564e9f89a5bbd2b56cd4f7fc47df338
Matheus Borri <matheus.escobar90@gmail.com>
2023-10-17 03:17:15 -0300

    finish ufo movement

--- style.js
+++ style.js
@@ -2,7 +2,13 @@ const modal = document.querySelector(".modal");
 const btnCloseModal = document.querySelector(".modal__btn-close");
 const overlay = document.querySelector(".overlay");
 const menu = document.querySelector(".menu");
+const ufo = document.querySelector(".menu__ufo");
 
+//Global variables
+let timer;
+let observedMenu = false;
+
+//Modal Toggle
 menu.addEventListener("click", (e) => {
   const card = e.target.closest(".card");
   if (card === null) return;
@@ -19,4 +25,34 @@ overlay.addEventListener("click", () => {
 btnCloseModal.addEventListener("click", () => {
   overlay.classList.toggle("hidden");
   modal.classList.toggle("hidden");
+});
+
+//Menu
+
+///UFO movement
+const ufoTilt = function (direction) {
+  setTimeout(() => {
+    ufo.style.animation = `tilt${direction} .2s linear forwards`;
+  }, 150);
+
+  clearTimeout(timer);
+  timer = setTimeout(function () {
+    ufo.style.animation = `untilt${direction} .2s linear forwards`;
+  }, 160);
+};
+
+menu.addEventListener("mousemove", function (e) {
+  const ufoWidth = ufo.offsetWidth;
+
+  setTimeout(() => {
+    ufo.style.left = `${e.pageX - ufoWidth / 2}px`;
+  }, 150);
+
+  if (e.movementX > 0) {
+    ufoTilt("Right");
+  }
+
+  if (e.movementX < 0) {
+    ufoTilt("Left");
+  }
 });

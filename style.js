99eed247d1fb743ef19a89b6b5569e5d4e498d1e
Matheus Borri <matheus.escobar90@gmail.com>
2023-10-18 01:20:45 -0300

    finish menu v1

--- style.js
+++ style.js
@@ -12,9 +12,32 @@ let observedMenu = false;
 menu.addEventListener("click", (e) => {
   const card = e.target.closest(".card");
   if (card === null) return;
+  const cardTitle = card.querySelector("h3").textContent;
+  const cardImg = card.querySelector("img").src.slice(21);
 
+  const modalContent = document.querySelector(".modal__content");
+
+  const HTML = `
+        
+
+        <img src="${cardImg}" alt="Burguer image" class="modal__img" />
+        <div class="modal__text-box">
+          <h3 class="heading__secondary modal__heading">${cardTitle}</h3>
+          <div class="modal__text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+            itaque autem nesciunt neque. Harum praesentium nulla eos aperiam
+            fugit et asperiores eaque, beatae odio obcaecati saepe ab libero
+            recusandae fuga?Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Minus, recusandae temporibus voluptas nihil repellat rerum
+            quia saepe officia a laborum perspiciatis architecto repellendus
+            distinctio enim autem pariatur explicabo modi mollitia!
+          </div>
+        </div>
+  `;
+
+  modalContent.innerHTML = HTML;
   modal.classList.toggle("hidden");
-  modal.classList.toggle("modal-visible");
+  /* modal.classList.toggle("modal-visible"); */
   overlay.classList.toggle("hidden");
 });
 
@@ -25,7 +48,7 @@ overlay.addEventListener("click", () => {
 
 btnCloseModal.addEventListener("click", () => {
   overlay.classList.toggle("hidden");
-  modal.classList.toggle("modal-visible");
+  /* modal.classList.toggle("modal-visible"); */
   modal.classList.toggle("hidden");
 });
 
